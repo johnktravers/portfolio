e71dd3b10b44c22399b7d0a0b628895668092707
John Travers <johnk.travers@gmail.com>
2020-06-01 07:42:26 -0400

    Add routes to about, projects, and home pages using react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import AboutPage    from './views/AboutPage';
+import ProjectsPage from './views/ProjectsPage';
+import HomePage     from './views/HomePage';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+        <Route path="/projects">
+          <ProjectsPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
