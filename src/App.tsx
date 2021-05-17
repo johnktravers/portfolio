33d257a193d23ecf57baabfd777999e6bcf66600
John Travers <johnk.travers@gmail.com>
2021-05-16 18:39:04 -0600

    Simplify main app by using new home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
 import React, {FC} from 'react'
-import {Container} from '@material-ui/core'
-import {makeStyles, ThemeProvider} from '@material-ui/core/styles'
 
-import Icons from './components/Icons'
-import Intro from './components/Intro'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import theme from './styles/theme'
+import Home from './pages'
 
-const useStyles = makeStyles({
-  appContainer: {
-  }
-})
-
-const App: FC = () => {
-  const classes = useStyles()
-
-  return (
-    <Container className={classes.appContainer}>
-      <ThemeProvider theme={theme}>
-        <Sidebar />
-        <Navbar />
-        <Intro />
-        <Icons />
-      </ThemeProvider>
-    </Container>
-  )
-}
+const App: FC = () => (
+    <>
+      <Home />
+    </>
+)
 
 export default App
